14040fee08fcbf524d662a70e9b71060cf6feca2
chan-a12 <ami159@cu.ac.kr>
2026-01-09 21:37:32 +0900

    fixed __init__

--- controller/services/__init__.py
+++ controller/services/__init__.py
@@ -2,7 +2,7 @@
 Services package for Kubernetes Backend Pod orchestration
 """
 
-from .backend_orchestrator import BackendOrchestrator
+from .pod_orchestrator import BackendOrchestrator
 
 __all__ = ['BackendOrchestrator']
 
