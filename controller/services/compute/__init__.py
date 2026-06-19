324db681d34011acab399368cb99e544a6b511a5
chan-a12 <ami159@cu.ac.kr>
2026-06-19 22:06:37 +0900

    Add cold-start compute pod allocation for E2 experiment

--- controller/services/compute/__init__.py
+++ controller/services/compute/__init__.py
@@ -1,6 +1,7 @@
 """Compute pod and compute-agent services"""
 
 from .agent_client import ComputeAgent, ComputeAgentError
+from .cold_start_pool import ColdStartComputePool
 from .cleanup import ComputeCleanup
 from .manager import ComputeManager
 from .warm_pod_pool import WarmPodPool, PodConflictError
@@ -8,6 +9,7 @@ from .warm_pod_pool import WarmPodPool, PodConflictError
 __all__ = [
     "ComputeAgent",
     "ComputeAgentError",
+    "ColdStartComputePool",
     "ComputeCleanup",
     "WarmPodPool",
     "ComputeManager",
